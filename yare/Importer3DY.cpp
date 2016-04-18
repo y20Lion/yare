6249c3ad4d1abeffa5172bfc16f6b5ee035d652b
yvain <yvain.raeymaekers@gmail.com>
2016-04-19 00:13:21 +0100

    first of read with jsoncpp

--- yare/Importer3DY.cpp
+++ yare/Importer3DY.cpp
@@ -2,11 +2,43 @@
 
 #include <fstream>
 
+#include <json/json.h>
+
 void import(const std::string& filename)
 {
-    std::ifstream file;
+	std::ifstream data_file("D:\\test.bin");
+	{
+		Json::Value root;
+		std::ifstream json_file("D:\\test.json");
+		json_file >> root;
+		json_file.close();
+		const auto& meshes = root["Meshes"];
+		for (const auto& mesh : meshes)
+		{
+			
+			auto val = mesh["VertexCount"].asInt();
+			auto val2 = mesh["TriangleCount"].asInt();
+
+			const auto& fields = mesh["Fields"];
+			for (const auto& field : fields)
+			{
+				const auto& datablock = field["DataBlock"];
+				auto address = datablock["Address"].asUInt64();
+				auto size = datablock["Size"].asUInt64();
+				data_file.seekg(address, std::ios::beg);
+				float* data = new float[size];
+				data_file.read((char*)data, sizeof(float) * 16);
+				int a = 0;
+				delete[] data;
+			}
+			
+		}
+	}
+	
+	
+	/*std::ifstream file;
     file.open(filename, std::ios::binary | std::ios::in);
-    float data[16];
+    
     file.read((char*)data, sizeof(float)*16);
-    int a = 0;
+    int a = 0;*/
 }
